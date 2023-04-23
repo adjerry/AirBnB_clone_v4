830e5bd8b4ac24cf7f0024c1d65fb086233f1c7d
root <adjerusa@gmail.com>
2023-04-23 10:44:02 -0700

    task

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,14 @@
+-- This sql script prepares a MySQL server for the project
+-- Creates a database
+-- creates a new uer in localhost
+-- sets users password
+-- grants ALL privilege on the created database
+-- grants SELECT privilege on the database performance_schema
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
